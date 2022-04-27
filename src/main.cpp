5ebfd11f03227d92e7acc7df1b5d68bf1516a702
Jas4118 <70446078+Jas4118@users.noreply.github.com>
2022-04-27 05:05:21 -0400

    Adding comments

--- src/main.cpp
+++ src/main.cpp
@@ -32,11 +32,11 @@ int main() {
 	
 	string courseNo, docNum;
 	cout << "Enter the course number. (1115, 3115, 3130)"<< endl;
-	cin >> courseNo;
+	cin >> courseNo; // user input
 	string skipLine, used;
 	outputFile.open("../outputFiles/"+ courseNo+ ".txt");
 
-	if ( courseNo == "1115" || courseNo == "3115" || courseNo == "3130"){
+	if ( courseNo == "1115" || courseNo == "3115" || courseNo == "3130"){ //check input
 		if(courseNo == "1115"){
 			docNum = "1115";
 			readFile.open("./csvFiles/1115.csv");
@@ -53,16 +53,16 @@ int main() {
 		cout << "Entered string is not valid" << endl;
 	}
 	outputFile << "Total student per prof \n\n";
-	printMap(findTotalStuProf(docNum,outputFile),outputFile);
+	printMap(findTotalStuProf(docNum,outputFile),outputFile); // prints map first and second
 	outputFile << '\n';
 
 	outputFile << "Number of W(ithdrawals) per prof \n\n";
-	printMap(wPerProf(docNum), outputFile);
+	printMap(wPerProf(docNum), outputFile); //prints wmap first and second
 	outputFile << '\n';
 
-	calcSpringPass(docNum, outputFile);
-	calcFallPass(docNum, outputFile);
-	calcCoursePass(docNum, outputFile);	
+	calcSpringPass(docNum, outputFile); //calculates rate of passing in Spring
+	calcFallPass(docNum, outputFile); //calculates rate of passing in Fall
+	calcCoursePass(docNum, outputFile); //calculates rate of passing overall
 }
 
 
