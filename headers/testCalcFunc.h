0e0a393e1c9c1916ffda0fd679a6dbd1d7e2a0f0
Jas4118 <70446078+Jas4118@users.noreply.github.com>
2022-04-27 03:18:57 -0400

    Update testCalcFunc.h

--- headers/testCalcFunc.h
+++ headers/testCalcFunc.h
@@ -1,9 +1,9 @@
 #ifndef TESTCALCFUNC_H
 #define TESTCALCFUNC_H
-#include <iostream>
+#include <iostream> 
 #include <string>
 #include <map>
-
+//functions used in main.cpp and testCalcFunc.cpp
 map<string,float> findTotalStuProf(string docNum);
 float calcCoursePass(string docNum,std::ofstream& outputFile);
 float calcSpringPass(string docNum,std::ofstream& outputFile);
